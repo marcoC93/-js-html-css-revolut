f388357a3b7305d431e65851b84b2cfe226dd718
Marco <marco.chianese93@gmail.com>
2020-10-28 17:45:50 +0100

    dropdown ( da correggere)

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
 
-      
+
+    $("li").mouseenter(function() {
+
+    $("ul").addClass("active");
+
+    }
+);
+
+    $("li").mouseleave(function() {
+    $("ul").removeClass("active");
+    });
 
 
 }) ;
