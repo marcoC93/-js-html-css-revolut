adc47236762502c1bea8d5bd1d486816417dede2
Marco <marco.chianese93@gmail.com>
2020-11-06 23:35:09 +0100

    restyling grafico css

--- script.js
+++ script.js
@@ -3,13 +3,26 @@ $(document).ready(function(){
 
     $("li").mouseenter(function() {
 
-    $("ul").addClass("active");
+        console.log("mouseenter");
 
-    }
-);
+        //$("ul").addClass("active");
+
+        // this : l'elemento che ha scatenato  l'evento
+        // children : trovare i figli diretti dell'elemento
+
+        $(this).find('ul').addClass('active');
+    });
 
     $("li").mouseleave(function() {
-    $("ul").removeClass("active");
+
+        console.log("mouseleave");
+
+
+
+        // this : l'elemento che ha scatenato  l'evento
+        // children : trovare i figli diretti dell'elemento
+
+        $(this).find('ul').removeClass('active');
     });
 
 
